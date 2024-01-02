9b52982530402d644e0b8bb214c75d3cecf23453
Jihong <jihong2253@gmail.com>
2024-01-02 04:40:59 -0500

    change footer

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -31,7 +31,7 @@ export default function BaseLayout() {
             <Grid item>
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                     py={'1.5rem'} sx={{opacity: 0.7}} width={'100%'}>
-                  <p>Jihong Park &copy; 2023</p>
+                  <p>Jihong Park &copy; 2024</p>
                </Box>
             </Grid>
          </Grid>
